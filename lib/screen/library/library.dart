23c99458f24068085c0ac69e241722d04896db85
weiwei146 <100007345+weiwei146@users.noreply.github.com>
2024-05-05 18:36:13 +0700

    set background

--- lib/screen/library/library.dart
+++ lib/screen/library/library.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:ui';
 import 'package:namer_app/screen/library/videoInReview.dart';
 
 class LibraryScreen extends StatefulWidget {
@@ -14,12 +15,36 @@ class _LibraryScreenState extends State<LibraryScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: SingleChildScrollView(
+    return Stack(children: [
+      Image.network(
+        'https://i.pinimg.com/564x/6b/66/51/6b6651cb9ca25ae29beea9502381f349.jpg',
+        fit: BoxFit.cover,
+        width: double.infinity,
+        height: double.infinity,
+      ),
+      Positioned.fill(
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+          child: Container(
+            color: Colors.white.withOpacity(0.2),
+          ),
+        ),
+      ),
+      Scaffold(
+    body: SingleChildScrollView(
+      child: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage(
+              'https://i.pinimg.com/564x/6b/66/51/6b6651cb9ca25ae29beea9502381f349.jpg',
+            ),
+            fit: BoxFit.cover,
+          ),
+        ),
             child: Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        SizedBox(height: 10.0),
+        SizedBox(height: 30.0),
         Row(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [searchBar(), SizedBox(width: 20.0)],
@@ -32,7 +57,7 @@ class _LibraryScreenState extends State<LibraryScreen> {
               "Hình ảnh",
               textAlign: TextAlign.left,
               style: TextStyle(
-                  fontWeight: FontWeight.bold, fontSize: 15, color: Colors.red),
+                  fontWeight: FontWeight.bold, fontSize: 17, color: Colors.white),
             ),
           ],
         ),
@@ -59,6 +84,8 @@ class _LibraryScreenState extends State<LibraryScreen> {
                   'https://nld.mediacdn.vn/291774122806476800/2022/9/9/z3707852783950752225873a84d1874063c8dc191319fa-16626983552691933008383.jpg'),
               SizedBox(width: 15.0),
               IconButton(
+                color: Colors.white,
+                iconSize: 30,
                 onPressed: () {
                   //xem full list ảnh ở đây
                 },
@@ -76,7 +103,7 @@ class _LibraryScreenState extends State<LibraryScreen> {
               "Video",
               textAlign: TextAlign.left,
               style: TextStyle(
-                  fontWeight: FontWeight.bold, fontSize: 15, color: Colors.red),
+                  fontWeight: FontWeight.bold, fontSize: 17, color: Colors.white),
             ),
           ],
         ),
@@ -98,6 +125,8 @@ class _LibraryScreenState extends State<LibraryScreen> {
               buildVideo(),
               SizedBox(width: 15.0),
               IconButton(
+                color: Colors.white,
+                iconSize: 30,
                 onPressed: () {
                   //xem full list ảnh ở đây
                 },
@@ -107,7 +136,7 @@ class _LibraryScreenState extends State<LibraryScreen> {
             ],
           ),
         ),
-        SizedBox(height: 40.0),
+        SizedBox(height: 50.0),
         Row(
           children: <Widget>[
             SizedBox(width: 20.0),
@@ -115,7 +144,7 @@ class _LibraryScreenState extends State<LibraryScreen> {
               "Tài liệu",
               textAlign: TextAlign.left,
               style: TextStyle(
-                  fontWeight: FontWeight.bold, fontSize: 15, color: Colors.red),
+                  fontWeight: FontWeight.bold, fontSize: 17, color: Colors.white),
             ),
           ],
         ),
@@ -142,6 +171,8 @@ class _LibraryScreenState extends State<LibraryScreen> {
                   'https://play-lh.googleusercontent.com/dl4ZuJhfD5xR9m2H-oZ9UcLZwYmGpmWMurPrvTiN831ZWLia9NbrYurXV-32wtOzPmM5'),
               SizedBox(width: 15.0),
               IconButton(
+                color: Colors.white,
+                iconSize: 30,
                 onPressed: () {
                   //xem full list ảnh ở đây
                 },
@@ -152,7 +183,8 @@ class _LibraryScreenState extends State<LibraryScreen> {
           ),
         ),
       ],
-    )));
+    ))))
+    ]);
   }
 
   Widget buildImage(String imageUrl) {
@@ -195,7 +227,7 @@ class _LibraryScreenState extends State<LibraryScreen> {
                   style: TextStyle(
                       fontSize: 12,
                       fontStyle: FontStyle.italic,
-                      color: Colors.black),
+                      color: Colors.white),
                 ),
               ],
             ),
@@ -238,7 +270,7 @@ class _LibraryScreenState extends State<LibraryScreen> {
                   style: TextStyle(
                       fontSize: 12,
                       fontStyle: FontStyle.italic,
-                      color: Colors.black),
+                      color: Colors.white),
                 ),
               ],
             ),
@@ -254,7 +286,7 @@ class _LibraryScreenState extends State<LibraryScreen> {
             searchController: controller,
             builder: (BuildContext context, SearchController controller) {
               return IconButton(
-                color: Colors.red,
+                color: Colors.white,
                 icon: const Icon(Icons.search),
                 onPressed: () {
                   // controller.openView();
@@ -290,7 +322,7 @@ class _LibraryScreenState extends State<LibraryScreen> {
   }
 
   Widget dialogImage(String imageUrl) {
-      return AlertDialog(
+    return AlertDialog(
       contentPadding: EdgeInsets.zero, // Đặt contentPadding thành zero
       content: IntrinsicWidth(
         child: IntrinsicHeight(
@@ -304,4 +336,8 @@ class _LibraryScreenState extends State<LibraryScreen> {
       ),
     );
   }
+
+  // Widget buildBackground(BuildContext context) {
+  //   return 
+  // }
 }
