02f38209937df2fc193fdd01b5a50269f27f6988
nghinn1012 <nth03.1012@gmail.com>
2024-05-10 18:18:24 +0700

    add firebase authentication. some bug in route page

--- lib/features/authentication/screens/login_page/widget/login_form_widget.dart
+++ lib/features/authentication/screens/login_page/widget/login_form_widget.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:namer_app/constants/sizes.dart';
+import 'package:get/get.dart';
+import 'package:namer_app/features/authentication/screens/forgot_password/widget/forgot_password_widget.dart';
+
+import '../../../../../constants/sizes.dart';
+import '../../../controllers/login_controller.dart';
+import '../../forgot_password/forgot_password_screen.dart';
 
 class LoginForm extends StatelessWidget {
   const LoginForm({
@@ -8,6 +13,10 @@ class LoginForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(LoginController());
+    bool showPassword =
+        false; // Tạo một biến để lưu trạng thái hiển thị mật khẩu
+
     return Form(
       child: Container(
         padding: const EdgeInsets.symmetric(vertical: tFormHeight - 10),
@@ -15,6 +24,7 @@ class LoginForm extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             TextFormField(
+              controller: controller.email,
               decoration: const InputDecoration(
                 prefixIcon: Icon(Icons.person_outline_outlined),
                 labelText: "Email",
@@ -24,14 +34,25 @@ class LoginForm extends StatelessWidget {
             ),
             const SizedBox(height: tFormHeight - 20),
             TextFormField(
-              decoration: const InputDecoration(
-                prefixIcon: Icon(Icons.fingerprint),
+              controller: controller.password,
+              validator: (value) {
+                if (value!.isEmpty) return 'Enter your password';
+                return null;
+              },
+              obscureText:
+                  !showPassword, // Sử dụng biến showPassword để quyết định việc hiển thị mật khẩu
+              decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.fingerprint),
                 labelText: "Password",
                 hintText: "Password",
-                border: OutlineInputBorder(),
                 suffixIcon: IconButton(
-                  onPressed: null,
-                  icon: Icon(Icons.remove_red_eye_sharp),
+                  onPressed: () {
+                    showPassword =
+                        !showPassword; // Đảo ngược trạng thái hiển thị mật khẩu khi người dùng nhấn vào
+                  },
+                  icon: Icon(
+                    showPassword ? Icons.visibility : Icons.visibility_off,
+                  ),
                 ),
               ),
             ),
@@ -39,7 +60,37 @@ class LoginForm extends StatelessWidget {
             Align(
               alignment: Alignment.centerRight,
               child: TextButton(
-                onPressed: () {},
+                onPressed: () {
+                  showModalBottomSheet(
+                    context: context,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0)),
+                    builder: (context) => Container(
+                      padding: const EdgeInsets.all(tDefaultSize),
+                      height: MediaQuery.of(context).size.height * 0.4,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Lựa chọn",
+                              style: Theme.of(context).textTheme.titleMedium),
+                          Text(
+                              "Lựa chọn phương thức bạn sẽ nhận thông tin thay đổi mật khẩu",
+                              style: Theme.of(context).textTheme.labelLarge),
+                          const SizedBox(height: 30.0),
+                          ForgetPasswordBtnWidget(
+                            onTap: () {
+                              Navigator.pop(context);
+                              Get.to(() => const ForgetPasswordMailScreen());
+                            },
+                            title: "Email",
+                            subTitle: "Thay đổi mật khẩu bằng email",
+                            btnIcon: Icons.mail_outline_rounded,
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
                 child: Text(
                   "Quên mật khẩu?",
                   style: Theme.of(context).textTheme.labelSmall,
@@ -56,7 +107,10 @@ class LoginForm extends StatelessWidget {
                   ),
                   padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
                 ),
-                onPressed: () {},
+                onPressed: () {
+                  controller
+                      .login(); // Gọi hàm login từ controller khi người dùng nhấn vào nút "Đăng nhập"
+                },
                 child: Text(
                   "Đăng nhập".toUpperCase(),
                   style: Theme.of(context).textTheme.labelMedium,
