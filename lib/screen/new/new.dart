0ddee4e5c6674501df3f5f60aae0a7fc9d5f1078
minhdatuet <minhdatuet@gmail.com>
2024-05-17 21:19:03 +0700

    update connect db new

---
+++ lib/screen/new/new.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import '../interface/SizeConfig.dart';
+import 'NewsCard.dart'; // Import NewsCard
+
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: NewScreen(),
+//     );
+//   }
+// }
+
+class NewsItem {
+  final String title;
+  final String imageUrl;
+
+  NewsItem({required this.title, required this.imageUrl});
+
+  factory NewsItem.fromDocument(DocumentSnapshot doc) {
+    return NewsItem(
+      title: doc['title'],
+      imageUrl: doc['imageUrl'],
+    );
+  }
+}
+
+class NewScreen extends StatefulWidget {
+  const NewScreen({super.key});
+
+  @override
+  State<NewScreen> createState() => _NewScreenState();
+}
+
+class _NewScreenState extends State<NewScreen> {
+  final List<NewsItem> newsItems = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchNewsItems();
+  }
+
+  void fetchNewsItems() async {
+    FirebaseFirestore.instance.collection('news').get().then((querySnapshot) {
+      querySnapshot.docs.forEach((doc) {
+        setState(() {
+          newsItems.add(NewsItem.fromDocument(doc));
+        });
+      });
+    }).catchError((error) {
+      print('Error fetching news items: $error');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Tin tức sự kiện',
+          style: TextStyle(
+            fontSize: 30, // Cỡ chữ to hơn
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: newsItems.isEmpty
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+        itemCount: newsItems.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0), // Padding trái phải
+            child: NewsCard(newsItem: newsItems[index]),
+          );
+        },
+      ),
+    );
+  }
+}
